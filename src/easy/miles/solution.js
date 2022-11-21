f247492e39348b5b354231b878b9a9c389eff27e
pooja <dhanwade.pooja2688@gmail.com>
2022-11-21 14:33:36 +0000

    miles

---
+++ src/easy/miles/solution.js
@@ -0,0 +1,9 @@
+// Convert kilometers to miles - assume 1.6 kilometers in
+//  a mile and round your answer to the nearest 
+//  mile (e.g. `1.6` should be `2`, as should `2.4`. `2.5` should be `3`).
+
+ let miles = 2.5
+ let roundUpMile=Math.floor(miles)
+ let roundDownMile=Math.ceil(miles)
+console.log(roundUpMile)
+console.log(roundDownMile)
