6ca6ce11dafe751adfedc7735c1bb63720d201bb
pooja <dhanwade.pooja2688@gmail.com>
2022-11-21 12:38:09 +0000

    3and5

--- src/easy/3or5/solution.js
+++ src/easy/3or5/solution.js
@@ -1,16 +1,13 @@
 // If the number  is divisible by 3, OR the number is divisible 5 under the value of 1000, return the value (into an array?) 
 
-let num = []
 
-for (let i = 0; i < 1000 ; i++) {
-    if ([i]/ 3 === 0 || [i]/ 5 === 0) {
-    num.push(i)}
-  } 
-  
-  console.log(num)
+let sum =0
+for (let i = 0; i< 1000 ; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        sum = sum + i
+    }
 
-
-// console.log(factors(1000))
+    }
+  console.log(sum)
 
 
-// add the sum of all the numbers that were returned in part 1
