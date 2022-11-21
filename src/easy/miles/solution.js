72ce52cb6e3f2f85ea2027c52f5ea787ac7cb1dc
pooja <dhanwade.pooja2688@gmail.com>
2022-11-21 15:25:05 +0000

    miles

--- src/easy/miles/solution.js
+++ src/easy/miles/solution.js
@@ -1,9 +1,18 @@
 // Convert kilometers to miles - assume 1.6 kilometers in
 //  a mile and round your answer to the nearest 
 //  mile (e.g. `1.6` should be `2`, as should `2.4`. `2.5` should be `3`).
+let miles 
+ let kilo
 
- let miles = 2.5
- let roundUpMile=Math.floor(miles)
- let roundDownMile=Math.ceil(miles)
-console.log(roundUpMile)
-console.log(roundDownMile)
+
+ function convertKilo(kilo){
+    let convert= kilo*0.6
+ console.log("convert",convert)
+console.log (convert.toFixed(3))
+    let roundDownMile=Math.floor(convert)
+    let roundUpMile=Math.round(convert)
+//    console.log(roundUpMile)
+//    console.log(roundDownMile)
+ return roundUpMile
+}
+console.log(convertKilo(3))
